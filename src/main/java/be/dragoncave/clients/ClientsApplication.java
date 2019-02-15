50fc42a05f83370ae7110cf1f1e7fc9fb4b63b8b
Benoit Goethals <Benoit.goethals@gmail.com>
2019-02-15 12:53:58 +0100

    update

--- src/main/java/be/dragoncave/clients/ClientsApplication.java
+++ src/main/java/be/dragoncave/clients/ClientsApplication.java
@@ -1,18 +1,11 @@
 package be.dragoncave.clients;
 
-import java.util.Date;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @EnableCaching
 @SpringBootApplication
